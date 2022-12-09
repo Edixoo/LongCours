06954681e0cd178adfb7a74224b4802212b4b377
Paul Pruvost <paul1pruvost@gmail.com>
2022-12-09 14:45:09 +0100

    SAE

--- inventaire.py
+++ inventaire.py
@@ -1,41 +1,62 @@
 class inventaire:
     
     def __init__(self) -> None:
-        self.inventaire={}
-        self.inventaire["gold"]=0
-        self.inventaire["textile"]=0
-        self.inventaire["petrole"]=0
-        self.inventaire["cereale"]=0
-        self.inventaire["bois"]=0
-        self.inventaire["machine-outil"]=0
+        self.gold=[];
+        self.textile=[];
+        self.bois=[];
+        self.petrole=[];
+        self.cereale=[];
+        self.machine_outil=[];
 
     def ajouter(self, march):
-        self.inventaire[march.nom]+=march.quantite
+        match march.nom:
+            case "or":
+                self.gold.append(march)
+            case "textile":
+                self.textile.append(march)
+            case "petrole":
+                self.petrole.append(march)
+            case "bois":
+                self.bois.append(march)
+            case "cereale":
+                self.cereale.append(march)
+            case "machine_outils":
+                self.machine_outil.append(march)
 
     def retirer(self, march):
-        self.inventaire[march.nom]-=march.quantite
+        match march.nom:
+            case "or":
+                self.gold.remove(march)
+            case "textile":
+                self.textile.remove(march)
+            case "petrole":
+                self.petrole.remove(march)
+            case "bois":
+                self.bois.remove(march)
+            case "cereale":
+                self.cereale.remove(march)
+            case "machine_outils":
+                self.machine_outil.remove(march)
 
-    def __str__(self):
+    def __str__(self) -> str:
         print("Contenu de l'inventaire:")
         resultat=""
-        for i in self.inventaire.keys():
-            resultat+=" - " + i +": " + str(self.inventaire[i]) + "\n"
+        if len(self.gold)==0:
+            resultat+="- Or: Vide \n"
+        else: 
+            resultat+="- Or: \n"
+            for i in self.gold:
+                resultat+="     - "+i
+        
+
+
+        # resultat=""
+        # resultat+=" - Or: " + str(self.gold) + "\n"
+        # resultat+=" - Pétrole: " + str(self.petrole) + "\n"
+        # resultat+=" - Céréale: " + str(self.cereale) + "\n"
+        # resultat+=" - Bois: " + str(self.bois) + "\n"
+        # resultat+=" - F: " + str(sel) + "\n"
         return resultat
-
-    def getOr(self):
-        return self.inventaire["gold"]
-
-    def getTextile(self):
-        return self.inventaire["textile"]
-    
-    def getPetrole(self):
-        return self.inventaire["petrole"]
-    
-    def getCereale(self):
-        return self.inventaire["cereale"]
-    
-    def getBois(self):
-        return self.inventaire["bois"]
-    
-    def getMachine_Outil(self):
-        return self.inventaire["machine-outil"]
+if __name__=="__main__":
+    r=inventaire()
+    r.ajouter
