c0d96f2ee95ddfc74ceefbc2a429b5425492e378
Akvir03 <nathan.devienne.pro@gmail.com>
2022-12-13 16:34:58 +0100

    Rectification marchandises

--- port.py
+++ port.py
@@ -11,6 +11,23 @@ class port:
 
         match type:
             case "cereale":
-
+                march=marchandises.cereale(0)
+                self.marchandise=march
+            case "gold":
+                march=marchandises.cereale(0)
+                self.marchandise=march
+            case "textile":
+                march=marchandises.cereale(0)
+                self.marchandise=march
+            case "petrole":
+                march=marchandises.cereale(0)
+                self.marchandise=march
+            case "bois":
+                march=marchandises.cereale(0)
+                self.marchandise=march
             case "machine_outils":
-                ...
+                march=marchandises.cereale(0)
+                self.marchandise=march
+            case _:
+                march=marchandises.marchandises(0,"Marchandise invalide","Pas de couleur")
+                print("Erreur de saisie")
