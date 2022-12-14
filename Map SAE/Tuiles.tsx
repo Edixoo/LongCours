72df06acb614b8d65bb63fbb432d9a79b4551e84
Akvir03 <nathan.devienne.pro@gmail.com>
2022-12-14 17:39:01 +0100

    Map en travaux

---
+++ Map SAE/Tuiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Tuiles" tilewidth="32" tileheight="48" tilecount="48" columns="8">
+ <image source="fantasyhextiles_v3.png" trans="000000" width="256" height="288"/>
+</tileset>
