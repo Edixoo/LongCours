a5f1c9de0211430807ed6a3670b8a5adfc53fba6
Akvir03 <nathan.devienne.pro@gmail.com>
2023-01-05 10:50:46 +0100

    Avancement main + map

--- world.py
+++ world.py
@@ -23,26 +23,50 @@ class world:
             for w in i.listeport:
                 w.marchandise.inflation()  
 
+    def definirjoueurs(self):
+        """Fonction de création des joueurs"""
+        nbjoueur = 0
+        while (nbjoueur<1 or nbjoueur>6):
+            print('Combien de joueurs désirez vous ?')
+            nbjoueur=input()
+        for i in range (nbjoueur):
+            print('Joueur',i+1,'Quel pseudo désirez vous ?')
+            pseudo=input()
+            couleur=0
+            print('Quelle couleur désirez vous ?')
+            couleur=input()
+            self.listejoueur.append(joueurs.joueur(pseudo,couleur))
+
     def distribuercarte(self) -> None:
-        """Fonction de distribution des cartes (utile au début de la partie)"""
-        nbcarte = len(self.jeudecarte.listecartemouv)+len(self.jeudecarte.listecartebdf)+len(self.jeudecarte.listecartetemp)
-        nbjoueur = len(self.listejoueur)
-        cartesbdf = self.jeudecarte.listecartebdf
-        cartesmouv = self.jeudecarte.listecartemouv
-        cartestemp = self.jeudecarte.listecartetemp
-        rd.shuffle(cartesbdf)
-        rd.shuffle(cartesmouv)
-        rd.shuffle(cartestemp)
-        if(nbcarte%nbjoueur!=0):
-            del cartesbdf[rd.randint(0,len(cartesbdf)-1)]
-        paquet=cartesbdf+cartesmouv+cartestemp
-        for i in range(0,len(paquet)):
-            numjou=0
-            self.listejoueur[numjou].listecartes.append(paquet[0])
-            del paquet[0]
-            numjou+=1
-            if (numjou==nbjoueur):
+            """Fonction de distribution des cartes (utile au début de la partie)"""
+            nbcarte = len(self.jeudecarte.listecartemouv)+len(self.jeudecarte.listecartebdf)+len(self.jeudecarte.listecartetemp)
+            nbjoueur = len(self.listejoueur)
+            cartesbdf = self.jeudecarte.listecartebdf
+            cartesmouv = self.jeudecarte.listecartemouv
+            cartestemp = self.jeudecarte.listecartetemp
+            rd.shuffle(cartesbdf)
+            rd.shuffle(cartesmouv)
+            rd.shuffle(cartestemp)
+            if(nbcarte%nbjoueur!=0):
+                del cartesbdf[rd.randint(0,len(cartesbdf)-1)]
+            paquet=cartesbdf+cartesmouv+cartestemp
+            for i in range(0,len(paquet)):
                 numjou=0
+                self.listejoueur[numjou].listecartes.append(paquet[0])
+                del paquet[0]
+                numjou+=1
+                if (numjou==nbjoueur):
+                    numjou=0
+
+    
+        
+
+
+
+    
+    
+
+            
 
 
 
