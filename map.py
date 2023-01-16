a4552fc57352b710aede1bdf0bda3349827a6904
judekabeya <judek9392@gmail.com>
2023-01-16 09:54:24 +0100

    backup

--- map.py
+++ map.py
@@ -1,8 +1,7 @@
 import pygame
 import port
 import portgraphique
-import InventaireGraphique
-import inventaire
+import CartesGraphique
 import marchandises
 
 class Map:
@@ -16,47 +15,30 @@ class Map:
         porte=port.port(1,"cereale","Le Cap")
         self.lecap=portgraphique.Portgraphique(porte,100,100, self.screen)
 
-        inv=inventaire.inventaire()
-        march=marchandises.gold(1550)
-        marchan=marchandises.machine_outils(5415)
-        inv.ajouter(marchan)
-        self.inventaire=InventaireGraphique.InventaireGraphique(self.screen, inv)
+        self.carte=CartesGraphique.Cartes(marchandises.cereale(150), 150, self.screen.get_width()/2, self.screen.get_height()/2, self.screen)
 
     def handling_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running= False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                print(pygame.mouse.get_pos())
                 self.lecap.checkforInput(pygame.mouse.get_pos())
-                self.inventaire.checkforInput(pygame.mouse.get_pos())
 
     def update(self):
         self.lecap.update(pygame.mouse.get_pos())
-        self.inventaire.update(pygame.mouse.get_pos())
 
     def display(self):
         image=pygame.image.load("./MapSAE/Test.png").convert()
         button_surface = pygame.transform.scale(image, (922, 800))
         self.screen.blit(button_surface,(0,0))
         self.lecap.display()
-        self.inventaire.display()
-
-        if self.lecap.isclicked:
+        self.carte.display()
+        if self.lecap.ishovered:
             self.lecap.afficher_interface()
         else:
             self.screen.blit(button_surface,(0,0))
             self.lecap.display()
-            self.inventaire.display()
-
-        
-        if self.inventaire.isclickedinv:
-            self.inventaire.afficher_inv()
-        else:
-            self.screen.blit(button_surface,(0,0))
-            self.lecap.display()
-            self.inventaire.display()
-
+            self.carte.display()
         pygame.display.flip()
 
     def run(self):
