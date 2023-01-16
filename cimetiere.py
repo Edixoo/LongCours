25a40e8497cf5e1037b8ca9a10c98f4b2047f095
Akvir03 <nathan.devienne.pro@gmail.com>
2023-01-16 09:24:39 +0100

    Modif cimetiere

--- cimetiere.py
+++ cimetiere.py
@@ -4,7 +4,7 @@ import joueurs
 
 class cimetiere:
     def __init__(self):
-        self.inventaire : list[inventaire.inventaire] = []
+        self.inventaire : inventaire.inventaire
         self.presence : list[joueurs.joueur] = []
         
     def recupechouage(self, bateau:bateau.bateau):
@@ -15,13 +15,13 @@ class cimetiere:
     def ramassercargaison(self, bateau:bateau.bateau):
         """Fonction permettant au bateau entré en paramètre de récupérer le contenu du cimetiere"""
         for i in range(len(self.inventaire)):
-            bateau.inventaire.gold+=self.inventaire[0].gold
-            bateau.inventaire.textile+=self.inventaire[0].textile
-            bateau.inventaire.bois+=self.inventaire[0].bois
-            bateau.inventaire.petrole+=self.inventaire[0].petrole
-            bateau.inventaire.cereale+=self.inventaire[0].cereale
-            bateau.inventaire.machine_outils+=self.inventaire[0].machine_outils
-            del self.inventaire[0]
+            bateau.inventaire.gold+=self.inventaire.gold
+            bateau.inventaire.textile+=self.inventaire.textile
+            bateau.inventaire.bois+=self.inventaire.bois
+            bateau.inventaire.petrole+=self.inventaire.petrole
+            bateau.inventaire.cereale+=self.inventaire.cereale
+            bateau.inventaire.machine_outils+=self.inventaire.machine_outils
+            
             
 
                 
